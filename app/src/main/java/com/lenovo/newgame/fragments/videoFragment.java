d171944354a249438fa05bea0f167cd6319244b5
MiLaN <milan.pokharna1998@gmail.com>
2018-06-22 22:39:59 +0530

    latest updates

--- app/src/main/java/com/lenovo/newgame/fragments/videoFragment.java
+++ app/src/main/java/com/lenovo/newgame/fragments/videoFragment.java
@@ -1,6 +1,5 @@
 package com.lenovo.newgame.fragments;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,15 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.lenovo.newgame.R;
 import com.lenovo.newgame.activity.video;
-import com.lenovo.newgame.adapters.RecyclerViewAdapterTwo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,19 +20,18 @@ import butterknife.Unbinder;
 
 public class videoFragment extends Fragment {
 
-    @BindView(R.id.Preschool)
-    TextView preschoolv;
+
+    @BindView(R.id.preschool)
+    LinearLayout preschool;
     @BindView(R.id.grade1)
-    TextView grade1v;
+    LinearLayout grade1;
     @BindView(R.id.grade2)
-    TextView grade2v;
+    LinearLayout grade2;
     @BindView(R.id.grade3)
-    TextView grade3v;
+    LinearLayout grade3;
     @BindView(R.id.grade4)
-    TextView grade4v;
+    LinearLayout grade4;
     Unbinder unbinder;
-
-
     public videoFragment() {
 
     }
@@ -85,37 +79,38 @@ public class videoFragment extends Fragment {
         unbinder.unbind();
     }
 
-    @OnClick({R.id.Preschool, R.id.grade1, R.id.grade2, R.id.grade3, R.id.grade4})
+    @OnClick({R.id.preschool, R.id.grade1, R.id.grade2, R.id.grade3, R.id.grade4})
     public void onViewClicked(View view) {
         Intent i;
         switch (view.getId()) {
-            case R.id.Preschool:
-                i = new Intent(getContext(),video.class);
-                i.putExtra("value","preschool");
+            case R.id.preschool:
+                i = new Intent(getContext(), video.class);
+                i.putExtra("value", "preschool");
                 startActivity(i);
                 break;
             case R.id.grade1:
                 i = new Intent(getContext(), video.class);
-                i.putExtra("value","grade1");
+                i.putExtra("value", "grade1");
                 startActivity(i);
                 break;
             case R.id.grade2:
                 i = new Intent(getContext(), video.class);
-                i.putExtra("value","grade2");
+                i.putExtra("value", "grade2");
                 startActivity(i);
                 break;
             case R.id.grade3:
                 i = new Intent(getContext(), video.class);
-                i.putExtra("value","grade3");
+                i.putExtra("value", "grade3");
                 startActivity(i);
                 break;
             case R.id.grade4:
                 i = new Intent(getContext(), video.class);
-                i.putExtra("value","grade4");
+                i.putExtra("value", "grade4");
                 startActivity(i);
                 break;
-                default:
-                    Toast.makeText(getContext(), "helooo guyss", Toast.LENGTH_SHORT).show();
+            default:
+                Toast.makeText(getContext(), "helooo guyss", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
